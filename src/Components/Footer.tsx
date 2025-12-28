647a5e4123b86703e5c83e489997dd85a2567c0a
rohitsingh <rohitsingh986491@gmail.com>
2025-12-28 18:11:18 +0530

    fixed padding

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='bg-gray-500 h-15 w-full text-center p-5'>
+    <div className='bg-gray-500 h-15 w-full text-center py-4'>
       <p>© Copyright Helping Hands Technologies. All Rights Reserved</p>
     </div>
   )
